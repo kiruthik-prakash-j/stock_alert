94e83e151acc9b950e8ebd564348b64578d51271
kiruthik-prakash-j <81346522+kiruthik-prakash-j@users.noreply.github.com>
2021-11-03 00:07:14 +0530

    Added messages.py file

---
+++ messages.py
@@ -0,0 +1,30 @@
+start_msg = """
+Hi ! I am Stock Bot
+I can help you get the following : 
+ - Stock News
+ - Stock Current Price
+ - Stock taget LOW/HIGH price
+ - Stock day LOW/HIGH
+ - Stock MarketCap
+ - Stock history
+"""
+
+help_msg = """
+To get the Stock News : 
+/news stock_name
+
+To get the target HIGH/LOW price
+/target stock_name
+
+To get the day HIGH/LOW
+/day stock_name
+
+To get the MarketCap
+/marketcap stock_name
+
+To get stock history
+/history stock_name
+
+To get the price at Intervals over a period
+/price stock_name interval period
+"""
